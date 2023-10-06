ab866e3e681143cd47d6f34c395132121ab5a287
haiderf7 <91637854+haiderf7@users.noreply.github.com>
2023-10-06 13:55:27 +0200

    new

---
+++ js/ui/displayMessage.mjs
@@ -0,0 +1,8 @@
+// DISPLAY MESSAGE
+
+export function displayMessage(messageType, message, target) {
+    const element = document.querySelector(target);
+  
+    element.innerHTML = `<div class="message ${messageType}">${message}</div>`;
+  }
+  
