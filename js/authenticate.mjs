d27cc26e7226b77b0a527a2008e185dbecaa33db
haiderf7 <91637854+haiderf7@users.noreply.github.com>
2023-10-12 23:21:02 +0200

    f

--- js/authenticate.mjs
+++ js/authenticate.mjs
@@ -1,28 +1,30 @@
 // REQUEST WITH TOKEN
 
 async function doFetch(url, method = `GET`) {
-    try {
-      console.log(url);
-      const token = (localStorage.getItem("token"));
-      console.log(token);
-      const fetchOptions = {
-        method: method,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token};
-        }`,
-        },
-      };
-      const response = await fetch(url, fetchOptions);
-      console.log(response);
-      const json = await response.json();
-      console.log(json);
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    console.log(url);
+    const token = (localStorage.getItem("token"));
+    console.log(token);
+    const fetchOptions = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:`Bearer ${token};
+        ,
+        ;
+      }`,
+      },
+    };
+    const response = await fetch(url, fetchOptions);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
   }
-  
-  const postsUrl = `${API_BASE_URL}/api/v1/social/posts`;
-  
-  doFetch(postsUrl);
-  
+}
+
+const postsUrl = `${API_BASE_URL}/api/v1/social/posts`;
+
+
+doFetch(postsUrl);
