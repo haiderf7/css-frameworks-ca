b8026f399922b3bbbda98ec383e2123062593191
haiderf7 <91637854+haiderf7@users.noreply.github.com>
2023-10-13 12:48:57 +0200

    e

--- js/ui/searchPosts.mjs
+++ js/ui/searchPosts.mjs
@@ -1,30 +1,33 @@
+// SEARCH POSTS
+
 import { renderPosts } from "./renderPosts.mjs";
 
-export function searchAndSortPosts(posts) {
+export function searchPosts(posts) {
   const search = document.querySelector(".search");
-  const sortCheckbox = document.querySelector("#testfilter");
 
-  search.addEventListener("input", function () {
-    const searchValue = search.value.trim().toLowerCase();
+  search.onkeyup = function (event) {
+    const searchValue = event.target.value.trim().toLowerCase();
 
     const filteredPosts = posts.filter((post) => {
-      return post.title.toLowerCase().includes(searchValue);
+      if (post.title.toLowerCase().includes(searchValue)) {
+        return true;
+      }
     });
-
-    if (sortCheckbox.checked) {
-      filteredPosts.sort((a, b) => a.title.localeCompare(b.title));
-    }
-
     renderPosts(filteredPosts);
-  });
+  };
+}
 
-  sortCheckbox.addEventListener("change", function () {
-    let filteredPosts = posts.slice();
-
-    if (sortCheckbox.checked) {
-      filteredPosts.sort((a, b) => a.title.localeCompare(b.title));
-    }
-
-    search.dispatchEvent(new Event('input')); // Re-run the search when sorting changes
-  });
+export function filterPosts(posts) {
+  search.onkeyup = function (event) {
+    posts.sort(function (a, b) {
+      if (a.firstname < b.firstname) {
+        return -1;
+      }
+      if (a.firstname > b.firstname) {
+        return 1;
+      }
+      return 0;
+    });
+    filterPosts(search);
+  };
 }
