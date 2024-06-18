5ea318fa5558dfee65e320f58371cd916de72a0f
haiderf7 <91637854+haiderf7@users.noreply.github.com>
2024-06-18 15:35:49 +0200

    add alert on delete post

--- js/deletePost.mjs
+++ js/deletePost.mjs
@@ -2,22 +2,28 @@
 
 import { authFetch } from "./api/authFetch.mjs";
 
-export default async function deletePost() {
-  const id = this.dataset.delete;
+export default async function deletePost(event) {
+  const postId = event.target.dataset.delete;
   const method = "DELETE";
 
-  if (id) {
-    const url = "https://api.noroff.dev/api/v1/social/posts/" + id;
+  if (postId) {
+    const url = `https://api.noroff.dev/api/v1/social/posts/${postId}`;
 
     try {
       const response = await authFetch(url, {
         method,
       });
-      const json = await response.json();
 
-      location.href = "index.html";
+      if (response.ok) {
+        alert("Post deleted successfully!"); // Alert message for successful deletion
+        location.href = "index.html"; // Redirect or update UI as needed
+      } else {
+        const json = await response.json();
+        throw new Error(json.message || "Failed to delete post");
+      }
     } catch (error) {
-      console.log("Error:", error);
+      console.error("Error deleting post:", error);
+      // Optionally handle errors or display error messages
     }
   }
 }
